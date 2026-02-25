96d5979c2a9fabc0c185f9cce85d6c5202a26136
Vishal <vishalpatel221302@gmail.com>
2026-02-25 10:40:39 +0530

    change url on server.js file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (!fs.existsSync(uploadsDir)) {
 // Middleware
 // ========================
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: ['https://kisan-krisi-kendra.vercel.app', 'https://kisan-krishi-kendra-admin.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
